14eaea3205a9a400dd597177c358c12aa6e582c4
Kevin Chung <kevin@weebly.com>
2015-03-17 18:52:31 +0000

    Force helm windows to take up entire width

--- init.el
+++ init.el
@@ -35,6 +35,8 @@
 
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
+(push '("^\*helm .+\*$" :regexp t) popwin:special-display-config)
+(push '("^\*helm-.+\*$" :regexp t) popwin:special-display-config)
 (popwin-mode 1)
 
 ;;
