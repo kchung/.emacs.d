b9af3b8e2f14d9e7170c2fccec885f49a78d6618
Kevin Chung <kevin@weebly.com>
2015-03-17 18:50:13 +0000

    Add helm

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(package-initialize)
+
 ;; Define package repositories
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
@@ -13,6 +15,20 @@
 (desktop-save-mode 1)
 
 ;;
+;; Helm
+;; =============
+
+(require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
 ;; Ibuffer Mode
 ;; ============
 
