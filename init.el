169dccd38f09e01945e6f8ec029ee57d8da4cb74
Kevin Chung <kevin@weebly.com>
2015-03-17 18:50:34 +0000

    Change helm-selection color for black terminals

--- init.el
+++ init.el
@@ -85,6 +85,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-selection ((t (:background "color-27" :foreground "brightwhite"))))
  '(magit-item-highlight ((t (:background "color-234"))))
  '(match ((t (:background "yellow")))))
 
