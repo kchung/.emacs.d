0bfefc2baf16038b56432b4e6c74d72dd93fa384
Kevin Chung <kevin@chun.gd>
2015-03-21 02:02:51 -0700

    Add web-mode package

--- init.el
+++ init.el
@@ -105,6 +105,15 @@
 (require 'scss-mode)
 (add-to-list 'auto-mode-alist '("\\.scss\\'"  . scss-mode))
 
+;; JSX
+(require `web-mode)
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
 ;;
 ;; custom-set-faces
 ;; =================
