37a8fa581b281342ea1583e603bbfc85f6ecb7d5
Kevin Chung <kevin@weebly.com>
2015-03-21 06:56:05 +0000

    Add tpl, php, and scss editing modes

--- init.el
+++ init.el
@@ -22,7 +22,6 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 
 (setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
@@ -84,6 +83,18 @@
 (make-directory "~/.emacs.d/autosaves/" t)
 (make-directory "~/.emacs.d/backups/" t)
 
+
+;;
+;; Editing modes
+;; =========
+
+(add-to-list 'auto-mode-alist '("\\.tpl\\'"   . html-mode))
+
+(require 'php-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'"   . php-mode))
+
+(require 'scss-mode)
+(add-to-list 'auto-mode-alist '("\\.scss\\'"  . scss-mode))
 
 ;;
 ;; custom-set-faces
